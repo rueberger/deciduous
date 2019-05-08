127d8a40438fbb0d67d4dfea8e6d4f798f9373f2
Andrew Berger <andbberger@gmail.com>
2019-05-07 21:21:26 -0400

    add some constants

---
+++ deciduous/src/board.rs
@@ -0,0 +1,16 @@
+/* Initial configuration of white */
+static WHITE_PIECES: i64 = 18446462598732840960;
+/* Initial configuration of black */
+static BLACK_PIECES: i64 = 65535;
+/* Initial configuration of pawns */
+static PAWNS: i64 = 71776119061282560;
+/* Initial configuration of bishops */
+static BISHOPS: i64 = 2594073385365405732;
+/* Initial confiuration of knights */
+static KNIGHTS: i64 = 4755801206503243842;
+/* The initla location of rooks */
+static ROOKS: i64 = 9295429630892703873;
+/* The initial location of the kings */
+static KINGS: i64 = 576460752303423496;
+/* The initial location of the queens */
+static QUEENS: i64 = 1152921504606846992;
