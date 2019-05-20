b3af1aa619077d4b1a45fe677c8536b198ff4a3b
Andrew Berger <andbberger@gmail.com>
2019-05-19 21:23:42 -0400

    fix compile-time errors, add Move struct, Color enum

--- deciduous/src/board.rs
+++ deciduous/src/board.rs
@@ -3,19 +3,71 @@
 /// As the chess board has 64 squares, we assign each square a bit, with the value of each bit determined by the
 /// occupancy of the corresponding square.
 
+// Some magic constants
 /* Initial configuration of white */
-static WHITE_PIECES: i64 = 18446462598732840960;
+static WHITE_PIECES: u64 = 18446462598732840960;
 /* Initial configuration of black */
-static BLACK_PIECES: i64 = 65535;
+static BLACK_PIECES: u64 = 65535;
 /* Initial configuration of pawns */
-static PAWNS: i64 = 71776119061282560;
+static PAWNS: u64 = 71776119061282560;
 /* Initial configuration of bishops */
-static BISHOPS: i64 = 2594073385365405732;
+static BISHOPS: u64 = 2594073385365405732;
 /* Initial confiuration of knights */
-static KNIGHTS: i64 = 4755801206503243842;
+static KNIGHTS: u64 = 4755801206503243842;
 /* The initla location of rooks */
-static ROOKS: i64 = 9295429630892703873;
+static ROOKS: u64 = 9295429630892703873;
 /* The initial location of the kings */
-static KINGS: i64 = 576460752303423496;
+static KINGS: u64 = 576460752303423496;
 /* The initial location of the queens */
-static QUEENS: i64 = 1152921504606846992;
+static QUEENS: u64 = 1152921504606846992;
+
+/** Returns an array of longs representing the board state.
+ *  Each long treated as a 64 bit word
+ *  Bit indexing:
+ *     Least significant digit is 0
+ *     MSD is 63
+ *
+ *  Square ordering:
+ *
+ *  1: A B C D E F G H | 0 1 2 3 4 5 6 7
+ *  2: A B C D E F G H | 8 9 10 11 12 13 14 15
+ *  3: A B C D E F G H | 16 17 18 19 20 21 22 23
+ *  ...
+ *  8: A B C D E F G H | 56 57 58 59 60 61 62 63
+ *
+ *  Array contents:
+ *
+ *  0:White pieces
+ *  1:Black pieces
+ *  2:pawns
+ *  3:bishops
+ *  4:knights
+ *  5:rooks
+ *  6:kings
+ *  7:queens  */
+fn init_bit_board() -> [u64; 8] {
+    let board: [u64; 8] = [
+        WHITE_PIECES,
+        BLACK_PIECES,
+        PAWNS,
+        BISHOPS,
+        KNIGHTS,
+        ROOKS,
+        KINGS,
+        QUEENS
+    ];
+    return board;
+}
+
+
+struct Move {
+    from: u8, // integer 1-64 TODO: zero indexing?
+    to: u8, // integer 1-64 TODO: zero indexing?
+    piece: u8, // an integer 2-7 representing the index in the bitboard that needs to be changed
+    color: Color
+}
+
+enum Color {
+    WHITE,
+    BLACK
+}
