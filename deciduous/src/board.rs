ae0e98b20a5c6dced8b3c85176c437927dbe093e
Andrew Berger <andbberger@gmail.com>
2019-05-19 22:40:52 -0400

    add Piece enum, wip make_move fn

--- deciduous/src/board.rs
+++ deciduous/src/board.rs
@@ -21,30 +21,30 @@ static KINGS: u64 = 576460752303423496;
 /* The initial location of the queens */
 static QUEENS: u64 = 1152921504606846992;
 
-/** Returns an array of longs representing the board state.
- *  Each long treated as a 64 bit word
- *  Bit indexing:
- *     Least significant digit is 0
- *     MSD is 63
- *
- *  Square ordering:
- *
- *  1: A B C D E F G H | 0 1 2 3 4 5 6 7
- *  2: A B C D E F G H | 8 9 10 11 12 13 14 15
- *  3: A B C D E F G H | 16 17 18 19 20 21 22 23
- *  ...
- *  8: A B C D E F G H | 56 57 58 59 60 61 62 63
- *
- *  Array contents:
- *
- *  0:White pieces
- *  1:Black pieces
- *  2:pawns
- *  3:bishops
- *  4:knights
- *  5:rooks
- *  6:kings
- *  7:queens  */
+/// Returns an array of longs representing the board state.
+/// Each long treated as a 64 bit word
+/// Bit indexing:
+///    Least significant digit is 0
+///    MSD is 63
+///
+/// Square ordering:
+///
+/// 1: A B C D E F G H | 0 1 2 3 4 5 6 7
+/// 2: A B C D E F G H | 8 9 10 11 12 13 14 15
+/// 3: A B C D E F G H | 16 17 18 19 20 21 22 23
+/// ...
+/// 8: A B C D E F G H | 56 57 58 59 60 61 62 63
+///
+/// Array contents:
+///
+/// 0:White pieces
+/// 1:Black pieces
+/// 2:pawns
+/// 3:bishops
+/// 4:knights
+/// 5:rooks
+/// 6:kings
+/// 7:queens
 fn init_bit_board() -> [u64; 8] {
     let board: [u64; 8] = [
         WHITE_PIECES,
@@ -59,15 +59,47 @@ fn init_bit_board() -> [u64; 8] {
     return board;
 }
 
+fn make_move(board: [u64; 8], m: Move) -> [u64; 8]{
+    match m.color {
+        Color::White => {
+        }
+        Color::Black => {
+        }
+    }
+}
+
 
 struct Move {
-    from: u8, // integer 1-64 TODO: zero indexing?
-    to: u8, // integer 1-64 TODO: zero indexing?
+    from: u8, // integer 0-63
+    to: u8, // integer 0-63
     piece: u8, // an integer 2-7 representing the index in the bitboard that needs to be changed
     color: Color
 }
 
 enum Color {
-    WHITE,
-    BLACK
+    White,
+    Black
+}
+
+enum Piece {
+    Pawn,
+    Bishop,
+    Knight,
+    Rook,
+    King,
+    Queen
+}
+
+impl Piece {
+    /// Returns the index of the bitboard for piece type
+    fn board_index(&self) -> u8 {
+        match self {
+            Piece::Pawn => 2,
+            Piece::Bishop => 3,
+            Piece::Knight => 4,
+            Piece::Rook => 5,
+            Piece::King => 6,
+            Piece::Queen => 7
+        }
+    }
 }
