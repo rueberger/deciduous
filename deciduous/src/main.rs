0ef95e9f0dc13dcd937fa772b46b0fa764dcc3e4
Andrew Berger <andbberger@gmail.com>
2020-01-22 15:08:48 -0800

    add little-endian test

--- deciduous/src/main.rs
+++ deciduous/src/main.rs
@@ -28,7 +28,8 @@ fn main() {
     let mut b = board::init_board();
     b.initialize();
 
-    println!("Whiten:\n{}", render_board(b.bitboard[0]));
+
+    println!("White:\n{}", render_board(b.bitboard[0]));
     println!("Black:\n{}", render_board(b.bitboard[1]));
     println!("Pawns:\n{}", render_board(b.bitboard[2]));
     println!("Bishops:\n{}", render_board(b.bitboard[3]));
@@ -36,4 +37,35 @@ fn main() {
     println!("Rooks:\n{}", render_board(b.bitboard[5]));
     println!("Kings:\n{}", render_board(b.bitboard[6]));
     println!("Queens:\n{}", render_board(b.bitboard[7]));
+
+    println!("a-file:\n{}", render_board(0x0101010101010101));
+    println!("1st-rank:\n{}", render_board(0x00000000000000FF));
+
+
+    // for idx in 0..8 {
+    //     println!("index: {} \n", idx);
+    //     println!("mask rank:\n{}", render_board(b.mask_rank[idx]));
+    //     println!("mask file:\n{}", render_board(b.mask_file[idx]));
+    //     println!("clear rank:\n{}", render_board(b.clear_rank[idx]));
+    //     println!("clear file:\n{}", render_board(b.clear_file[idx]));
+    // }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn is_little_endian() -> bool {
+        if cfg!(target_endian = "little") {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    #[test]
+    fn test_little_endian() {
+        assert!(is_little_endian());
+    }
 }
