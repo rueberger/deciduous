1b35aa8b463e08fb484a00e766319d297c082927
Andrew Berger <andbberger@gmail.com>
2020-01-25 22:52:30 -0800

    fix bad_argsort, change signature

--- deciduous/src/utils.rs
+++ deciduous/src/utils.rs
@@ -3,12 +3,12 @@
 
 // TODO: there must be something in std that does this
 /// crappy arg sort that only works efficiently for unique values in tiny ranges
-pub bad_argsort(arr: [Option<u8>]) -> Vec<u8> {
+pub fn bad_argsort(arr: Vec<Option<u8>>) -> Vec<u8> {
     let mut sorted_vals = Vec::new();
 
     for idx in 0..arr.len() {
         if let Some(val) = arr[idx] {
-            indices.put(val as u8)
+            sorted_vals.push(val as u8)
         }
     }
 
