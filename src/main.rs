d6ae9c9a71e0e0d27ae9c140e14884388efe09e1
Andrew Berger <andbberger@gmail.com>
2025-06-16 15:45:26 -0600

    sets main to run perft

--- src/main.rs
+++ src/main.rs
@@ -2,39 +2,22 @@ mod board;
 mod moves;
 mod utils;
 
-fn render_board(state: u64) -> String {
-    let mut render = String::new();
+use std::time;
 
-    // TODO: no idea if this order is right
-    for rank in (0..8).rev() {
-        for file in 0..8 {
-            let idx = board::square_index(rank, file);
-
-            render.push('[');
-            // TODO: check for order here too
-            if let Some(_) = moves::bitscan_lsd(state & (1 << idx)) {
-                render.push('X')
-            } else {
-                render.push(' ')
-            }
-            render.push(']')
-        }
-        render.push('\n')
+fn m_perft(depth: usize) {
+    println!("| depth | nodes | time |");
+    for d in 1..(depth + 1) {
+        let start = time::Instant::now();
+        let nodes = moves::perft(d);
+        let elapsed = start.elapsed();
+        let secs = elapsed.as_secs();
+        let ms = elapsed.subsec_millis();
+        println!("| {d} | {nodes} | {secs}.{ms} |");
     }
-    return render;
 }
 
 fn main() {
-    let mut b = board::init_board();
-    let mut move_gen = moves::init_move_gen();
-    move_gen.initialize();
-
-    println!("White:\n{}", render_board(b.own_pieces));
-    b.color_flip();
-    println!("White flipped:\n{}", render_board(b.own_pieces));
-    println!("{}", moves::serialize_board(b.own_pieces).as_slice()[12]);
-
-    let xs: [i32; 5] = [1, 2, 3, 4, 5];
+    m_perft(5);
 }
 
 #[cfg(test)]
